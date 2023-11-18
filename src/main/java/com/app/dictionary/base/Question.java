17f9f68e74d80d0a63dbe84a569c59f2daf49fff
Princessdoll7 <princessdoll1710@gmail.com>
2023-11-18 21:26:17 +0700

    add question

---
+++ src/main/java/com/app/dictionary/base/Question.java
@@ -0,0 +1,81 @@
+package com.app.dictionary.base;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question {
+    private String question;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private String correctAnswer;
+
+    public Question(String question, String answer1, String answer2, String answer3, String answer4, String correctAnswer) {
+        this.question = question;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public static List<Question> loadQuestion() throws IOException {
+        List<Question> questions = new ArrayList<Question>();
+        FileReader fileReader = new FileReader("txt/question.txt.txt");
+        BufferedReader reader = new BufferedReader(fileReader);
+        String tmp;
+        while ((tmp = reader.readLine()) != null) {
+            String[] lines = tmp.split("=");
+            System.out.println(lines.length);
+            if(lines.length == 6) {
+                Question question = new Question(lines[0], lines[1], lines[2], lines[3], lines[4], lines[5]);
+                questions.add(question);
+            }
+        }
+        return questions;
+    }
+
+    public static void main(String[] args) {
+        try {
+            List<Question> questions = loadQuestion();
+            System.out.println(questions.size());
+            for (Question question : questions) {
+                System.out.println(question.getQuestion());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
