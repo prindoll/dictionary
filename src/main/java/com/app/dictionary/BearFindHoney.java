543e9d890874c77e7ef2fbe69ce3ee09d1f58661
Thucdzio <letienthuc7b@gmail.com>
2023-11-20 09:22:34 +0700

    fix game

--- src/main/java/com/app/dictionary/BearFindHoney.java
+++ src/main/java/com/app/dictionary/BearFindHoney.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
@@ -48,13 +49,19 @@ public class BearFindHoney extends GameController implements Initializable {
     private Button ButtonKq;
     @FXML
     private ImageView imageWIN;
+    @FXML
+    private ImageView imageQuestion1, imageQuestion2, imageQuestion3, imageQuestion4,imageQuestion5,imageQuestion6;
     final double[] rock = {400, 0, 400, 80, 240, 80, 240, 240, 80, 160, 0
             , 160, 0, 320, 0, 400, 160, 400, 320, 480, 400, 320};
-    final double[] ques = {160, 80, 320, 160, 80, 320, 160, 320, 320, 400, 160, 480};
+    final double[] quesConst = {160, 80, 320, 160, 80, 320, 160, 320, 320, 400, 160, 480};
+    double [] ques  = new double[quesConst.length];
+
     private double step = 80.0;
 
     private final Image imgBear = new Image(getClass().getResourceAsStream("img/bear.png"));
     private final Image imgWin = new Image(getClass().getResourceAsStream("img/win.png"));
+    private final Image imgPass = new Image(getClass().getResourceAsStream("img/pass.png"));
+    private final Image imgQues = new Image(getClass().getResourceAsStream("img/q.png"));
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -64,7 +71,17 @@ public class BearFindHoney extends GameController implements Initializable {
     }
 
     public void load() {
+
         bear.setImage(imgBear);
+        imageQuestion1.setImage(imgQues);
+        imageQuestion2.setImage(imgQues);
+        imageQuestion3.setImage(imgQues);
+        imageQuestion4.setImage(imgQues);
+        imageQuestion5.setImage(imgQues);
+        imageQuestion6.setImage(imgQues);
+        for (int i = 0 ; i < quesConst.length; i++){
+            ques[i] = quesConst[i];
+        }
     }
 
     public void move() {
@@ -113,19 +130,22 @@ public class BearFindHoney extends GameController implements Initializable {
             }
             bear.setLayoutX(x);
             bear.setLayoutY(y);
-            if (isQuestion(x, y)) {
-
+            if (isQuestion(x, y) != 0) {
                 turnOnToAll();
                 setStep(0);
 
             }
             if (ButtonKq.getText().equals(correct)) {
+                int tmp = isQuestion(x, y);
+                setPass(tmp);
+                ques[(tmp-1)*2] = -1;
+                ques[(tmp-1)*2 + 1] = -1;
                 setStep(80);
                 hideToAll();
-                
+
             }
             if (isWin()) {
-
+                youWin();
             }
         });
         anchorPane.setFocusTraversable(true);
@@ -147,16 +167,16 @@ public class BearFindHoney extends GameController implements Initializable {
         bear.setLayoutX(x += 0);
     }
 
-    public boolean isQuestion(double x, double y) {
+    public int  isQuestion(double x, double y) {
         double valX, valY;
         for (int i = 0; i < ques.length; i += 2) {
             valX = ques[i];
             valY = ques[i + 1];
             if (x == valX && y == valY) {
-                return true;
+                return i / 2 + 1;
             }
         }
-        return false;
+        return 0;
     }
 
     public void checkButton(Question q) {
@@ -234,18 +254,44 @@ public class BearFindHoney extends GameController implements Initializable {
         answer3.setVisible(false);
         answer4.setVisible(false);
         ButtonKq.setVisible(false);
+        ButtonKq.setText(wrong);
     }
 
     public boolean isWin() {
         if (bear.getLayoutX() == 400 && bear.getLayoutY() == 480) return true;
         return false;
     }
-
+    public void setPass(int index) {
+        if ( index == 1) {
+            imageQuestion1.setImage(imgPass);
+        }
+        else if ( index  == 2) {
+            imageQuestion2.setImage(imgPass);
+        }
+        else if ( index  == 3) {
+            imageQuestion3.setImage(imgPass);
+        }
+        else if ( index  == 4) {
+            imageQuestion4.setImage(imgPass);
+        }
+        else if ( index  == 5) {
+            imageQuestion5.setImage(imgPass);
+        }
+        else if ( index  == 6) {
+            imageQuestion6.setImage(imgPass);
+        }
+    }
     public void setStep(double step) {
         this.step = step;
     }
-    public void cloe() {
-        System.exit(0);
+    public void youWin() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Thông báo");
+        alert.setHeaderText(null);
+        alert.setContentText("Bạn xứng đáng có 10 ny");
+
+
+        alert.showAndWait();
     }
 }
 
