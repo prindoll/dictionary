06d6b0bd8c304519c3436c59cfdd69cb542e0c54
QuangTran1011 <quangtran101104@gmail.com>
2023-11-21 20:23:57 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/app/dictionary/BearFindHoney.java
    #	src/main/resources/com/app/dictionary/fxml/BearFindHoney.fxml

--- src/main/java/com/app/dictionary/Hangman.java
+++ src/main/java/com/app/dictionary/Hangman.java
@@ -1,5 +1,6 @@
 package com.app.dictionary;
 
+import com.app.dictionary.base.Question;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,11 +15,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
-import java.util.Random;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Hangman extends GameController implements Initializable {
     @FXML
@@ -50,6 +51,10 @@ public class Hangman extends GameController implements Initializable {
     @FXML
     private TextField tf8;
     @FXML
+    private TextField tf9;
+    @FXML
+    private TextField tf10;
+    @FXML
     private TextField input;
     @FXML
     private Label hint;
@@ -58,49 +63,44 @@ public class Hangman extends GameController implements Initializable {
     @FXML
     private Label textwin = new Label("");
 
-    private String[] data = {
-            "MEXICO COUNTRY",
-            "HEDWIG BIRD",
-            "KUAKATA BEACH",
-            "CANADA COUNTRY",
-            "DOCTOR PROFESSION",
-            "FOOTBALL GAME",
-            "TEACHER MENTOR",
-            "LEOPARD ANIMAL",
-            "BICYCLE TRANSPORT",
-            "SALMON FISH",
-            "SPARROW BIRD",
-            "PARROTS BIRD",
-            "EAGLE BIRD",
-            "TRAIN TRANSPORT",
-            "SHIP TRANSPORT",
-            "ENGINEER PROFESSION",
-            "BANKER PROFESSION",
-            "CRICKET GAME"
-    };
+    private int random;
+    private String word_hint;
+    private String[] split;
+    private String word;
+    private String hint_str;
+    private int letter_size;
+    private int life = 6;
 
-    private int random ;
-    private String word_hint ;
-    private String[] split ;
-    private String word ;
-    private String hint_str ;
-    private int letter_size ;
-    private int life=6;
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){
-        setWord();
-        setHint();
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            loadData();
+            setWord();
+            setHint();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    List<String> words = new ArrayList<String>();
+    public void loadData() throws IOException {
+        FileReader fileReader = new FileReader("txt/game.txt");
+        BufferedReader reader = new BufferedReader(fileReader);
+        String tmp;
+        while ((tmp = reader.readLine()) != null) {
+            words.add(tmp);
+        }
+    }
     public void setWord() {
-        random = new Random().nextInt(data.length);
-        word_hint = data [random];
-        split = word_hint.split(" ", 2);
+        random = new Random().nextInt(words.size());
+        word_hint = words.get(random);
+        split = word_hint.split(" - ", 2);
         word = split[0];
         hint_str = split[1];
         letter_size = word.length();
     }
-    public void setHint(){
+
+    public void setHint() {
         hint.setText(hint_str);
         tf1.setText("");
         tf2.setText("");
@@ -110,37 +110,56 @@ public class Hangman extends GameController implements Initializable {
         tf6.setText("");
         tf7.setText("");
         tf8.setText("");
+        tf9.setText("");
+        tf10.setText("");
 
         tf5.setVisible(true);
         tf6.setVisible(true);
         tf7.setVisible(true);
         tf8.setVisible(true);
-        if(letter_size==7){
-            tf8.setVisible(false);
+        tf9.setVisible(true);
+        tf10.setVisible(true);
+        if (letter_size == 9) {
+            tf10.setVisible(false);
         }
-        if(letter_size==6){
+        if (letter_size == 8) {
+            tf9.setVisible(false);
+            tf10.setVisible(false);
+        }
+        if (letter_size == 7) {
+            tf8.setVisible(false);
+            tf9.setVisible(false);
+            tf10.setVisible(false);
+        }
+        if (letter_size == 6) {
             tf7.setVisible(false);
             tf8.setVisible(false);
+            tf9.setVisible(false);
+            tf10.setVisible(false);
         }
-        if(letter_size==5){
+        if (letter_size == 5) {
             tf6.setVisible(false);
             tf7.setVisible(false);
             tf8.setVisible(false);
+            tf9.setVisible(false);
+            tf10.setVisible(false);
         }
-        if(letter_size==4){
+        if (letter_size == 4) {
             tf5.setVisible(false);
             tf6.setVisible(false);
             tf7.setVisible(false);
             tf8.setVisible(false);
+            tf9.setVisible(false);
+            tf10.setVisible(false);
         }
     }
 
 
-    public void CheckInput(){
+    public void CheckInput() {
         String str = input.getText().toUpperCase();
         if (word.contains(str)) {
             int index = 0;
-            for(int i=0; i<word.length(); i++) {
+            for (int i = 0; i < word.length(); i++) {
                 char c = word.charAt(i);
                 if (String.valueOf(c).equals(str)) {
                     setLetter(index, Character.toString(c));
@@ -148,44 +167,43 @@ public class Hangman extends GameController implements Initializable {
                 index++;
             }
 
-        }
-        else {
+        } else {
             setImage();
         }
     }
 
-    public void setLetter(int index,String str){
-        if(index==0)
+    public void setLetter(int index, String str) {
+        if (index == 0)
             tf1.setText(str);
-        else if(index==1)
+        else if (index == 1)
             tf2.setText(str);
-        else if(index==2)
+        else if (index == 2)
             tf3.setText(str);
-        else if(index==3)
+        else if (index == 3)
             tf4.setText(str);
-        else if(index==4)
+        else if (index == 4)
             tf5.setText(str);
-        else if(index==5)
+        else if (index == 5)
             tf6.setText(str);
-        else if(index==6)
+        else if (index == 6)
             tf7.setText(str);
-        else if(index==7)
+        else if (index == 7)
             tf8.setText(str);
     }
 
-    public void setImage(){
+    public void setImage() {
         life--;
-        if(life==5)
+        if (life == 5)
             img.setImage(image2);
-        else if(life==4)
+        else if (life == 4)
             img.setImage(image3);
-        else if(life==3)
+        else if (life == 3)
             img.setImage(image4);
-        else if(life==2)
+        else if (life == 2)
             img.setImage(image5);
-        else if(life==1)
+        else if (life == 1)
             img.setImage(image6);
-        else if(life==0)
+        else if (life == 0)
             img.setImage(image7);
     }
 
